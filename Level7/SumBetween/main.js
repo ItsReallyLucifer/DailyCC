fc64a851c61c4aae973b0295abff31a37403a0d0
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-04-06 12:17:06 -0700

    change

--- Level7/SumBetween/main.js
+++ Level7/SumBetween/main.js
@@ -1,6 +1,6 @@
 // Create a function that takes two parameters (numbers)
 // Find the sum of all the numbers between and including them and return it.
-// If both numbers are the same, return either or..  
+// If both numbers are the same, return either or.
 // Example: (-1,2) --> 2   (-1 + 0 + 1 + 2 = 2)
 
 function getSum(a, b){
