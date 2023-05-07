027df9e0911d6d3a5502cd3b615cf6f03b98a1da
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-05-07 11:06:01 -0700

    remove first and last char

---
+++ Level8/RemoveFL/main.js
@@ -0,0 +1,10 @@
+// create a function that takes in a string
+// & removes the first and last character of the string.
+
+
+
+function removeChar(str) {
+    return str.slice(1, -1);
+}
+
+console.log(removeChar('Javascript')) // avascrip
