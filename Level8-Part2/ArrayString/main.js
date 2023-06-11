064525e75591cef0afe67c6980bf90fbf7709664
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-06-11 16:31:14 -0700

    udpate

--- Level8-Part2/ArrayString/main.js
+++ Level8-Part2/ArrayString/main.js
@@ -1,5 +1,5 @@
-// Create a function that takes an array of words and combines them together into a sentence
-// Example ['hello', 'world'] => 'hello world'
+// Create a function that takes an array of words and combines them together into a sentence.
+// Example ['hello', 'world'] => 'hello world' 
 
 
 function combines(words) {
