eed23082d5ca3e747d387e7c9f1e105b0a90fa4d
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-06-21 16:05:17 -0700

    daily update

---
+++ Level8-Part2/NegativeNum/main.js
@@ -0,0 +1,17 @@
+// Create a function that takes in a number and returns the negative.
+// But take into account negative parameters and also 0
+
+
+
+
+
+function makeNegative(num) {
+    if ( num > 0 ) {
+          return (num * -1);
+      }
+      else {
+          return num;
+      }
+  }
+
+  console.log(makeNegative(5))
