9aace63cd284ece230b1d882ee221b0edaf7c02c
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-06-24 16:29:22 -0700

    daily update

---
+++ Level8-Part2/ArrayString/main.js
@@ -0,0 +1,9 @@
+// Create a function that takes an array of words and combines them together into a sentence
+// Example ['hello', 'world'] => 'hello world'
+
+
+function combines(words) {
+    return words.join(' ')
+}
+
+console.log(combines(['did', 'it', 'work']))
