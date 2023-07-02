be79ac08141f6afc71344202491912e496bba54d
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-07-02 12:26:09 -0700

    change

--- Level8/Summation/main.js
+++ Level8/Summation/main.js
@@ -1,9 +1,10 @@
-//Write a program(function) that finds the summation of every number from 1 to num. 
+//Create a function that finds the summation of every number from 1 to a num. 
 //The number will always be a positive integer greater than 0.
+//example: 5 --> 1 + 2 + 3 + 4 + 5 = 15
 
 
  function summation(num){
         return (num * (num + 1)) / 2
     }
 
-    console.log(summation(10))
+    console.log(summation(5))
