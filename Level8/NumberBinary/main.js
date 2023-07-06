d12e5319d4dbebe719071a98cd7333dfb688a043
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-07-05 20:54:08 -0700

    daily update

---
+++ Level8/NumberBinary/main.js
@@ -0,0 +1,7 @@
+// create a function that adds two numbers together and returns their sum in binary.
+
+function addBinary(a,b){
+    let addition = a + b
+    return addition.toString(2)
+}
+console.log(addBinary(30,20))
