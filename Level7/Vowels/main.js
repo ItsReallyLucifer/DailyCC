6ef4dcbdb9a8e7ff7b182d6c4704a65f52410cf3
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-07-07 22:42:36 -0700

    daily update

---
+++ Level7/Vowels/main.js
@@ -0,0 +1,10 @@
+// Return the number (count) of vowels in the given string.
+// Consider a,e,i,o,u but not y
+
+function getCount(str) {
+    return str.split('').filter(c => "aeiouAEIOU".includes(c)).length;
+}
+
+    let str = "hello world";
+    let vowelCount = getCount(str);
+    console.log(vowelCount); // Output: 3
