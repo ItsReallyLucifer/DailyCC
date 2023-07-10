990c0199fd4e0f20ddc49b1c72b30bff6d12f93d
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-07-09 20:37:17 -0700

    daily update

---
+++ Level8/CountbyX/main.js
@@ -0,0 +1,12 @@
+// Create a function that takes 2 parameters that will return an array.
+// The array returned should run 'n' number of times, in increments of 'x'
+
+
+function countBy(x, n) {
+    var z = [];
+    for (i = 1; i <= n; i++) {
+        z.push(x * i);
+    }
+    return z;
+}
+console.log(countBy(10,20))
