a0088913c4585107f5ce6170b034131ac6a4fd45
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-07-10 15:32:27 -0700

    updated

--- Level8/SmallestInteger/main.js
+++ Level8/SmallestInteger/main.js
@@ -1,31 +1,12 @@
-//Given an array of integers your solution should find the smallest integer.
+//Create a function that given an array of integers your solution should find the smallest integer.
 //Given [34, 15, 88, 2] your solution will return 2
 
 
-class SmallestIntegerFinder {
-    findSmallestInt(args) {
-    return Math.min.apply(Math, args);
-    }
-  }
+function findSmallestInt(nums){
+ let solution = Math.min.apply(Math, nums)
+ return solution
+}
 
-  // to input the integers and console.log it... 
-//   const finder = new SmallestIntegerFinder();
-//   const numbers = [7, 2, 9, 1, 5];
-//   console.log(finder.findSmallestInt(numbers)); // Output: 1
-
-
-
-
-//solution using spread operator
-// class SmallestIntegerFinder {
-//   findSmallestInt(args) {
-//     return Math.min(...args)
-//   }
-// }
-
-
-  //Or, without the class of SmallestIntegerFinder
-  
-//   function findSmallestInt(args) {
-//     return Math.min.apply(Math, args);
-//   }
+const numbers = [75,54,62,20]
+const smallestInt = findSmallestInt(numbers)
+console.log(smallestInt)
