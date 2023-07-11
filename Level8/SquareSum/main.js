d0c3b645aeea358b8520974005bc0009d7e393a8
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-07-10 17:00:12 -0700

    pseudo change

--- Level8/SquareSum/main.js
+++ Level8/SquareSum/main.js
@@ -1,5 +1,5 @@
 //Create a function that squares each number passed into it and then sums the results together.
-//example: [1, 2, 2] should return 9 .. because 1^2 + 2^2 + 2^2 = 9
+//example: [1, 2, 2] should return 9 .. because 1^2 + 2^2 + 3^2 = 14
 
 
 function squaredSum(numbers){
