d0a19e578f7a406906440d70ef90181f63bb9ee2
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-07-15 19:57:15 -0700

    change

--- Level8/ReturnOpp/main.js
+++ Level8/ReturnOpp/main.js
@@ -1,4 +1,4 @@
-//  given an integer or a floating-point number, find its opposite.
+//Create a function that given an integer or a floating-point number, it'll find and return its opposite.
 
 function opposite(number) {
     let answer = number * -1
