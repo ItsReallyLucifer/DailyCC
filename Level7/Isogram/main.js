1b77f238133a648df5d4da2b256590bf413dd5ba
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-07-21 15:43:48 -0700

    daily update

---
+++ Level7/Isogram/main.js
@@ -0,0 +1,9 @@
+// Create a function that determines whether a string that contains only letters is an isogram.
+// example: moose = false (the letter o twice.)
+
+
+function isIsogram(str){
+	return new Set(str.toUpperCase()).size == str.length;
+}
+
+console.log(isIsogram('strings'))
