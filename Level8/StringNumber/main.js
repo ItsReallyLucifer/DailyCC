fb1da4ca8445e23f2078634fe49c7a1d0216db69
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-07-25 12:34:03 -0700

    change

--- Level8/StringNumber/main.js
+++ Level8/StringNumber/main.js
@@ -1,12 +1,5 @@
-// Create a function that can transform a string into a number.
+// Create a function that can transform a string into a number..
 
-// function stringToNum(str){
-//     return parseInt(str)
-// }
-// console.log(stringToNum('25'))
-
-
-//or
 
 function stringToNum(str){
     return Number(str)
